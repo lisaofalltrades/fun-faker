774979e18c30792f14da8397deb94fd764cf2c6d
the crafty coder <lisathuanguyen@gmail.com>
2020-06-20 01:02:19 -0700

    office - random character & random quote

---
+++ office.js
@@ -0,0 +1,43 @@
+// Variables
+const characters = [
+  'Michael Scott', 'Dwight Schrute', 'Jim Halpert', 'Pam Beesly', 'Ryan Howard', 'Andy Bernard', 'Robert California'
+]
+
+const quotes = [
+  'Sometimes I\'ll start a sentence and I don\'t even know where it\'s going. I just hope I find it along the way.',
+  'I talk a lot, so I\'ve learned to just tune myself out...',
+  'I am Beyonce always.',
+  'Oh, it is on, like a prawn who yawns at dawn.',
+  'I am running away from my responsibilities and it feels good.',
+  'Should have burned this place down when I had the chance.',
+  'One day Michael came in and complained about a speed bump on the highway. I wonder who he ran over then.',
+  'Mini cupcakes? As in the mini version of regular cupcakes? Which is already a mini version of cake? Honestly, where does it end with you people?',
+  'I don\'t care what they say about me. I just want to eat.',
+  'I want people to be afraid of how much they love me.',
+  'Whenever I\'m about to do something, I think, \'Would an idiot do that?\' and if they would, I do not do that thing.',
+  'I stopped caring a long time ago.',
+  'It takes an advanced sense of humor. I don\'t expect everybody to understand.',
+  'The doctor said, if I can\'t find a new way to relate more positively to my surroundings, I\'m going to die. I\'m going to die.',
+  'I\'m not superstitious, but I am a little stitious.',
+  'You only live once? False. You live every day. You only die once.',
+  'Bears, beets, Battlestar Galactica',
+  'I understand nothing.',
+  '"What are your weaknesses?" "I don\'t have any, a--hole."',
+  'I got six numbers. One more would have been a complete telephone number.',
+  'For my new year\'s resolution, I gave up drinking...during the week.',
+  'An office is a place where dreams come true.',
+  'I wish there was a way to know you\'re in the good old days before you\'ve actually left them.',
+  'I am one of the few people who looks hot eating a cupcake.',
+  'If I don\'t have some cake soon, I might die.',
+  'I knew exactly what to do, but in a much more real sense I had no idea what to do.',
+  'Sorry I annoyed you with my friendship.',
+  'I just wanna lie on the beach and eat hot dogs. That\'s all I\'ve ever wanted.',
+  'That\'s what she said.'
+]
+function randomCharacter () {
+  return characters[Math.floor(Math.random() * characters.length)]
+}
+
+function randomQuote () {
+  return quotes[Math.floor(Math.random() * quotes.length)]
+}
