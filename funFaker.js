2346df0b822f8b4f82c257998e226ff26cc48047
the crafty coder <lisathuanguyen@gmail.com>
2020-06-24 21:03:48 -0700

    fixing typo in userEmail()

--- funFaker.js
+++ funFaker.js
@@ -6,6 +6,9 @@ module.exports = {
   randomItem: (args) => {
     return args[Math.floor(Math.random() * args.length)]
   },
+  userEmail: (character) => {
+    return (character[0] + character.split(' ').pop()).toLowerCase() + '@email.com'
+  },
   office: office,
   hp: hp,
   medical: medical
